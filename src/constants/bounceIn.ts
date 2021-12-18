a0f4248edef13bf85393cd8f98e4f9fd4eef5581
klodian shaba <dev.klodian@gmail.com>
2021-12-18 14:53:22 +0100

    add bounce in as constant

---
+++ src/constants/bounceIn.ts
@@ -0,0 +1,15 @@
+import {animate, keyframes, style, transition, trigger} from "@angular/animations";
+
+export const bounceInKeyframe = [
+  style({ opacity: 0 , transform: 'scale3d(0.3, 0.3, 0.3)', offset: 0}),
+  style({ transform: 'scale3d(1.1, 1.1, 1.1)', offset: .2}),
+  style({transform: 'scale3d(0.9, 0.9, 0.9)', offset: .4}),
+  style({ opacity: 1, transform: 'scale3d(1.03, 1.03, 1.03)', offset: .6}),
+  style({ transform: 'scale3d(0.97, 0.97, 0.97)', offset: .8}),
+  style({ transform: 'none', offset: 1} ),
+];
+
+export const  bounceIn = trigger('bounceIn', [
+    transition('* => in', animate('{{timings}}', keyframes( bounceInKeyframe ))),
+    transition(':enter', animate('{{timings}}', keyframes( bounceInKeyframe ))),
+  ]);
