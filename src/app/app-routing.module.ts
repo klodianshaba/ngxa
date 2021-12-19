66281dc562ffdc0b33c23930193579bc5edd717a
klodian shaba <dev.klodian@gmail.com>
2021-12-19 21:04:12 +0100

    change routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule } from '@angular/core';
+import {RouterModule, Routes } from '@angular/router';
+import {NotFoundComponent} from "./shared/not-found/not-found.component";
+import {LandingLayoutComponent} from "./layouts/landing-layout/landing-layout.component";
+import {DashboardLayoutComponent} from "./layouts/dashboard-layout/dashboard-layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch:'full', redirectTo:'landing'},
+  {path: '', component: LandingLayoutComponent,
+    children: [
+        { path:'landing', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule) }
+      ]
+  },
+  {path: '', component: DashboardLayoutComponent,
+    children: [
+      {path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m =>m.DashboardModule) }
+    ]
+  },
+  {path:'**' , component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from "./shared/shared.module";
+import { LandingLayoutComponent } from "./layouts/landing-layout/landing-layout.component";
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingLayoutComponent,
+    DashboardLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
