0c0bde24f19f7ef6f675b6282451bed837f961b0
klodian shaba <dev.klodian@gmail.com>
2021-12-19 21:03:33 +0100

    change bounce in

--- src/constants/bounceIn.ts
+++ src/constants/bounceIn.ts
@@ -1,6 +1,6 @@
 import {animate, keyframes, style, transition, trigger} from "@angular/animations";
 
-export const bounceInKeyframe = [
+export const bounceInKeyframes = [
   style({ opacity: 0 , transform: 'scale3d(0.3, 0.3, 0.3)', offset: 0}),
   style({ transform: 'scale3d(1.1, 1.1, 1.1)', offset: .2}),
   style({transform: 'scale3d(0.9, 0.9, 0.9)', offset: .4}),
@@ -10,6 +10,6 @@ export const bounceInKeyframe = [
 ];
 
 export const  bounceIn = trigger('bounceIn', [
-    transition('* => in', animate('{{timings}}', keyframes( bounceInKeyframe ))),
-    transition(':enter', animate('{{timings}}', keyframes( bounceInKeyframe ))),
-  ]);
+    transition('* => in', animate('{{timings}}', keyframes( bounceInKeyframes ))),
+    transition(':enter', animate('{{timings}}', keyframes( bounceInKeyframes ))),
+]);
