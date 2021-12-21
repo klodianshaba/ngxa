c8caea8333602e54e2dc97de0b78861c507e55cc
klodian shaba <dev.klodian@gmail.com>
2021-12-21 19:53:36 +0100

    change bounceIN

--- src/app/components/description/description.component.ts
+++ src/app/components/description/description.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {zoomIn, zoomInClick} from "../../library/zoomIn";
+import {bounceIn} from "../../library";
 
 @Component({
   selector: 'app-description',
   templateUrl: './description.component.html',
-  styleUrls: ['./description.component.scss']
+  styleUrls: ['./description.component.scss'],
+  animations: [
+    zoomIn(),
+    bounceIn(),
+    zoomInClick(),
+  ]
 })
 export class DescriptionComponent implements OnInit {
 
+  status: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  animate() {
+    this.status = false;
+    setTimeout(() => {
+      this.status = true;
+    }, 1);
   }
 
 }
--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {bounceIn} from "../../library";
+import {trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    bounceIn()
+  ]
 })
 export class DashboardComponent implements OnInit {
 
+  public ani: boolean = false;
+
   constructor() { }
 
+
+
   ngOnInit(): void {
+  }
+  onClick(): void{
+    this.ani = true;
+    setTimeout(()=>{
+      this.ani = false;
+    },800);
   }
 
 }
