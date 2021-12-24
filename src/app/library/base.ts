90014c46625a96e47ab7392db2e243d1ae1ed2e3
klodian shaba <dev.klodian@gmail.com>
2021-12-24 12:47:18 +0100

    base changed

--- src/app/library/base.ts
+++ src/app/library/base.ts
@@ -1,21 +1,46 @@
-import {AnimationConfig, DefaultAnimationConfig, DefaultTimings, NestedAnimations} from "./common";
-import {animate, animateChild, animation, AnimationAnimateMetadata, AnimationReferenceMetadata, AnimationTransitionMetadata, AnimationTriggerMetadata, group, keyframes, query, transition, trigger, useAnimation} from "@angular/animations";
+import {AnimationConfig, DefaultAnimationConfig, NestedAnimations} from "./common";
+import {
+  animate,
+  animateChild,
+  animation,
+  AnimationAnimateMetadata,
+  AnimationOptions,
+  AnimationReferenceMetadata,
+  AnimationTransitionMetadata,
+  AnimationTriggerMetadata,
+  group,
+  keyframes,
+  query,
+  transition,
+  trigger,
+  useAnimation
+} from "@angular/animations";
 import {zoomInKeyframes} from "./zoomIn";
 
-export function buildTrigger(animationReferenceMetadata: AnimationReferenceMetadata ,config?: Partial<AnimationConfig>): AnimationTriggerMetadata {
+export function buildTrigger(animationReferenceMetadata: AnimationReferenceMetadata ,config: Partial<AnimationConfig> = DefaultAnimationConfig, options?: AnimationOptions | null): AnimationTriggerMetadata {
   let configs: AnimationConfig = {...DefaultAnimationConfig, ...config };
-  return trigger(configs.triggerName, buildTransitions(configs, animationReferenceMetadata));
+  return trigger(configs.triggerName, buildTransitions(animationReferenceMetadata,configs,options));
 }
 
-export function buildTransitions(config: AnimationConfig, animationReferenceMetadata: AnimationReferenceMetadata): AnimationTransitionMetadata[]{
+export function buildTransitions( animationReferenceMetadata: AnimationReferenceMetadata, config: AnimationConfig, options?: AnimationOptions | null): AnimationTransitionMetadata[]{
   return [
-    transition(config.stateChangeExpression.toString(),
-      includeNestedAnimations( animationReferenceMetadata, config)
-    )
+    transition(
+      includeStateChangeExpressions(config.stateChangeExpressions),
+      includeNestedAnimations( animationReferenceMetadata, config),
+      includeOptions(config, options)
+      )
   ]
 }
 
-function includeNestedAnimations(animation: AnimationReferenceMetadata, config?: Partial<AnimationConfig>) {
+export function includeOptions(config: AnimationConfig, options?: AnimationOptions | null): AnimationOptions | null{
+  return  {delay:options?.delay , params: {...{delay: config.delay, timings: config.timings}, ...options?.params } }
+}
+
+export function includeStateChangeExpressions(stateChangeExpressions: AnimationConfig['stateChangeExpressions']): string{
+   return (stateChangeExpressions instanceof Array) ? stateChangeExpressions.join() : stateChangeExpressions;
+}
+
+export function includeNestedAnimations(animation: AnimationReferenceMetadata, config?: Partial<AnimationConfig>) {
   return [
     ...(config && config.nestedAnimations === NestedAnimations.before ? [query('@*', animateChild(), { optional: true })] : []),
     group([ useAnimation(animation),
