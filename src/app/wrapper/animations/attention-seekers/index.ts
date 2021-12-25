7a14acecdf85fd510eb8b66a4cc4739eb1c96c39
klodian shaba <dev.klodian@gmail.com>
2021-12-25 21:41:55 +0100

    add index in attention seekers

---
+++ src/app/wrapper/animations/attention-seekers/index.ts
@@ -0,0 +1,8 @@
+export * from './bounce';
+export * from './flash';
+export * from './head-shake';
+export * from './heart-beat';
+export * from './jello';
+export * from './swing';
+export * from './wobble';
+
