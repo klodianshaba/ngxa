1961efdf09aa8b05841e05f155ada010c9159e2a
klodian shaba <dev.klodian@gmail.com>
2021-12-25 21:36:26 +0100

    create bounce function

---
+++ src/app/wrapper/animations/attention-seekers/bounce.ts
@@ -0,0 +1,22 @@
+import {animate, keyframes, style, AnimationTriggerMetadata, animation, AUTO_STYLE} from "@angular/animations";
+import {buildTrigger} from "../../base";
+import {AnimationConfig} from "../../common";
+
+export const bounceKeyframes = [
+  style({ opacity: 0, visibility: AUTO_STYLE, transform: 'translate3d(0, 0, 0)', easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)', offset: 0 }),
+  style({ opacity: 1, transform: 'translate3d(0, 0, 0)', easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)', offset: 0.2 }),
+  style({ transform: 'translate3d(0, -30px, 0)', easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)', offset: 0.4 }),
+  style({ transform: 'translate3d(0, -30px, 0)', easing: 'cubic-bezier(0.755, 0.05, 0.855, 0.06)', offset: 0.43 }),
+  style({ transform: 'translate3d(0, 0, 0)', easing: 'cubic-bezier(0.755, 0.05, 0.855, 0.06)', offset: 0.53 }),
+  style({ transform: 'translate3d(0, -15px, 0)', easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)', offset: 0.7 }),
+  style({ transform: 'translate3d(0, 0, 0)', easing: 'cubic-bezier(0.755, 0.05, 0.855, 0.06)', offset: 0.8 }),
+  style({ transform: 'translate3d(0, -4px, 0)', easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)', offset: 0.9 }),
+  style({ transform: 'translate3d(0, 0, 0)', easing: 'ease', offset: 1 })
+];
+
+export function bounce(config?: Partial<AnimationConfig>): AnimationTriggerMetadata {
+  return buildTrigger(
+    animation( animate('{{timings}} {{delay}}', keyframes(bounceKeyframes) )),
+    {...{triggerName: 'bounce'},...config}
+  )
+}
