61025d18ade761d5156fa5b2f36389e91d1c4515
klodian shaba <dev.klodian@gmail.com>
2021-12-27 18:20:51 +0100

    add animations list into sidebar navigation

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {zoomIn, bounceIn} from "../../wrapper";
+import {AnimationModel } from "../../wrapper/animations/animations";
+import {animations, Animations} from "../../wrapper/animations/animations";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
@@ -7,14 +10,17 @@ import {zoomIn, bounceIn} from "../../wrapper";
   styleUrls: ['./sidebar.component.scss'],
   animations: [
     zoomIn(),
-    bounceIn({stateChangeExpressions:[':enter', '0 => 1']})
+    bounceIn({stateChangeExpressions:[':enter', '0 => 1']}),
+    animations(),
   ]
 })
 export class SidebarComponent implements OnInit {
 
   status:boolean = false;
 
-  constructor() { }
+  public animations: AnimationModel[] = Animations;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -24,6 +30,28 @@ export class SidebarComponent implements OnInit {
     setTimeout(() => {
       this.status = true;
     }, 1);
+  }
+  onHover(animation: AnimationModel): void{
+    if(!animation.canAnimate) {
+      this.onAnimateIt(animation);
+    }
+    animation.canAnimate = true;
+  }
+  onLeave(animation: AnimationModel): void{
+     animation.canAnimate = false;
+  }
+  onClick(animation: AnimationModel): void{
+    this.onAnimateIt(animation);
+    this.router.navigate(['dashboard'],{queryParams:{animation:animation.triggerName}}).then();
+  }
+
+  onAnimateIt(animation: AnimationModel): void{
+      animation.value = false;
+      setTimeout(() => {
+        const index = this.animations.indexOf(animation);
+        animation.value = animation.triggerName;
+        this.animations[index] = Object.assign({}, this.animations[index]);
+      }, 1);
   }
 
 }
