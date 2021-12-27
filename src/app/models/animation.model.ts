7478b560fd92942c0c3bf6cc372be161ecab2eb3
klodian shaba <dev.klodian@gmail.com>
2021-12-27 18:24:21 +0100

    add animation model into animations file

--- src/app/models/animation.model.ts
+++
@@ -1,7 +0,0 @@
-import {AnimationOptions} from "@angular/animations";
-
-export interface AnimationModel{
-  triggerName:string,
-  value: string | number | boolean,
-  params: AnimationOptions | null
-}
--- src/app/wrapper/animations/animations.ts
+++ src/app/wrapper/animations/animations.ts
@@ -3,13 +3,34 @@ import {buildTrigger} from "../base";
 import {AnimationConfig} from "../common";
 import {bounceInTransition} from "./bounceIn";
 import {zoomInAnimation, zoomInTransition} from "./zoomIn";
-import {
-  bounceTransition,
-  flashTransition,
-  headShakeTransition,
-  heartBeatTransition, jelloTransition,
-  swingTransition
-} from "./attention-seekers";
+import {bounceTransition, flashTransition, headShakeTransition, heartBeatTransition, jelloTransition, swingTransition} from "./attention-seekers";
+import {pulseTransition} from "./attention-seekers/pulse";
+import {rubberBandTransition} from "./attention-seekers/rubber-band";
+import {shakeTransition} from "./attention-seekers/shake";
+
+import {AnimationOptions} from "@angular/animations";
+
+export interface AnimationModel{
+  triggerName:string,
+  value: string | number | boolean,
+  params: AnimationOptions | null,
+  canAnimate: boolean,
+  active: boolean
+}
+
+export const Animations: AnimationModel[] = [
+  {triggerName:'zoomIn', value: 'zoomIn' ,params:{}, canAnimate: false, active: false},
+  {triggerName:'bounceIn', value: 'bounceIn' ,params:{}, canAnimate: false,active: false},
+  {triggerName:'swing', value: 'swing' ,params:{}, canAnimate: false,active: false},
+  {triggerName:'bounce', value: 'bounce' ,params:{}, canAnimate: false,active: false},
+  {triggerName:'flash', value: 'flash' ,params:{}, canAnimate: false,active: false},
+  {triggerName:'headShake', value: 'headShake' ,params:{}, canAnimate: false,active: false},
+  {triggerName:'heartBeat', value: 'heartBeat' ,params:{}, canAnimate: false,active: false},
+  {triggerName:'jello', value: 'jello' ,params:{}, canAnimate: false,active: false},
+  {triggerName:'pulse', value: 'pulse' ,params:{}, canAnimate: false,active: false},
+  {triggerName:'rubberBand', value: 'rubberBand' ,params:{}, canAnimate: false,active: false},
+  {triggerName:'shake', value: 'shake' ,params:{}, canAnimate: false,active: false},
+];
 
 export function animations(config?: Partial<AnimationConfig>): AnimationTriggerMetadata{
   return buildTrigger(
@@ -23,7 +44,10 @@ export function animations(config?: Partial<AnimationConfig>): AnimationTriggerM
         flashTransition({...config, ...{stateChangeExpressions:'* => flash'}}),
         headShakeTransition({...config, ...{stateChangeExpressions:'* => headShake'}}),
         heartBeatTransition({...config, ...{stateChangeExpressions:'* => heartBeat'}}),
-        jelloTransition({...config, ...{stateChangeExpressions:'* => jello'}})
+        jelloTransition({...config, ...{stateChangeExpressions:'* => jello'}}),
+        pulseTransition({...config, ...{stateChangeExpressions:'* => pulse'}}),
+        rubberBandTransition({...config, ...{stateChangeExpressions:'* => rubberBand'}}),
+        shakeTransition({...config, ...{stateChangeExpressions:'* => shake'}})
       ]
     }
   );
