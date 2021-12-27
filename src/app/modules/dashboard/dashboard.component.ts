34806f9e0c8113e624e74424f117bf91e2943403
klodian shaba <dev.klodian@gmail.com>
2021-12-27 18:19:59 +0100

    dashboard view animations and animation with query params

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,53 +1,72 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
-import { zoomIn, bounceIn, listAnimateChild} from "../../wrapper";
-import {AnimationModel} from "../../models/animation.model";
-import {swing,wobble,bounce,flash,headShake,heartBeat,jello} from "../../wrapper/animations/attention-seekers";
-import {animations} from "../../wrapper/animations/animations";
+import {listAnimateChild} from "../../wrapper";
+import {AnimationModel} from "../../wrapper/animations/animations";
+import {Animations, animations} from "../../wrapper/animations/animations";
+import {ActivatedRoute, Router} from "@angular/router";
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   animations: [
     listAnimateChild(),
-    // bounceIn({stateChangeExpressions:[':enter', '0 => 1']}),
-    // zoomIn({stateChangeExpressions:[':enter', '0 => 1']}),
-    // swing({stateChangeExpressions:[':enter', '0 => 1']}),
-    // wobble({stateChangeExpressions:[':enter', '0 => 1']}),
-    // bounce({stateChangeExpressions:[':enter', '0 => 1']}),
-    // flash({stateChangeExpressions:[':enter', '0 => 1'], timings:'1000ms'}),
-    // headShake({stateChangeExpressions:[':enter', '0 => 1']}),
-    // heartBeat({stateChangeExpressions:[':enter', '0 => 1']}),
-    // jello({stateChangeExpressions:[':enter', '0 => 1']}),
-
-    animations({stateChangeExpressions:':enter, 0 => 1'}),
+    animations(),
  ]
 })
 export class DashboardComponent implements OnInit {
 
-  bounceIn: string = 'bounceIn';
-  public animations: AnimationModel[] = [
-    {triggerName:'zoomIn', value: 'zoomIn' ,params:{}},
-    {triggerName:'bounceIn', value: 'bounceIn' ,params:{}},
-    {triggerName:'swing', value: 'swing' ,params:{}},
-    {triggerName:'bounce', value: 'bounce' ,params:{}},
-    {triggerName:'flash', value: 'flash' ,params:{}},
-    {triggerName:'headShake', value: 'headShake' ,params:{}},
-    {triggerName:'heartBeat', value: 'heartBeat' ,params:{}},
-    {triggerName:'jello', value: 'jello' ,params:{}},
-  ];
+  public animations: AnimationModel[] = Animations;
+  public animation: AnimationModel | undefined;
 
-  constructor(private cdf: ChangeDetectorRef) { }
+  constructor(private cdf: ChangeDetectorRef, private activatedRoute: ActivatedRoute, private router: Router) {
+    this.activatedRoute.queryParams.subscribe(params =>{
+      this.animation = this.animations.find(item => item.triggerName === params['animation']);
+      if(this.animation){
+        this.onAnimateCard(this.animation);
+      }
+      this.checkActiveAnimation();
+    });
+  }
 
   ngOnInit(): void {}
-  onAnimateIt(animation: AnimationModel): void{
+
+  onAnimateCard(animation: AnimationModel): void{
+    this.animation = this.animations.find(item => item.triggerName === animation.triggerName);
+    if(this.animation) {
+      this.animation.value = false;
+      setTimeout(() => {
+        if(this.animation) {
+          this.animation.value = this.animation.triggerName;
+          this.animation = Object.assign({}, this.animation);
+        }
+      }, 1);
+    }
+  }
+
+  onAnimateCards(animation: AnimationModel): void{
     animation.value = false;
     setTimeout(() => {
       const index = this.animations.indexOf(animation);
       animation.value = animation.triggerName;
       this.animations[index] = Object.assign({}, this.animations[index]);
-    },1);
+      },1);
   }
 
+  onViewIt(animation: AnimationModel): void{
+    this.router.navigate(['dashboard'],{queryParams:{animation:animation.triggerName}}).then();
+  }
+  onViewAnimations(){
+    this.router.navigate(['dashboard']).then();
+    this.animation = undefined;
+    this.checkActiveAnimation();
+  }
 
-
+  checkActiveAnimation(): void{
+    this.animations.map( item => {
+        if(item.triggerName === this.animation?.triggerName){
+          return Object.assign(item ,{active: true});
+        }
+        return Object.assign(item ,{active: false});
+      }
+    );
+  }
 }
